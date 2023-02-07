0c863b00983c4f7cecfd532fc57b5151ea84da99
João Mozelli Neto <joaomozelli@hotmail.com>
2023-02-07 17:41:05 -0300

    turn navbar fixed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,18 +4,19 @@ import './styles.css';
 const Header = () => {
   return (
     <header>
-      <nav>
-        <div className="nav-wrapper orange accent-4">
-          <Link to="/" className="brand-logo">Mozelli Marketing</Link>
-          <Link to="/" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></Link>
-          <ul className="right hide-on-med-and-down">
-            <li><Link to="/">Página Inicial</Link></li>
-            <li><Link to="/">Categorias</Link></li>
-            <li><Link to="/">Contato</Link></li>
-          </ul>
-        </div>
-      </nav>
-
+      <div className="navbar-fixed">
+        <nav>
+          <div className="nav-wrapper orange accent-4">
+            <Link to="/" className="brand-logo">Mozelli Marketing</Link>
+            <Link to="/" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></Link>
+            <ul className="right hide-on-med-and-down">
+              <li><Link to="/">Página Inicial</Link></li>
+              <li><Link to="/">Categorias</Link></li>
+              <li><Link to="/">Contato</Link></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
       <ul className="sidenav" id="mobile-demo">
         <li><Link to="/">Página Inicial</Link></li>
         <li><Link to="/">Categorias</Link></li>
